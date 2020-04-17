038b485c117c01e3f06171a20ce41fae9a9d346e
Mike Caine <mikey.caine@btinternet.com>
2020-04-17 14:10:30 +0100

    added componenet did mount

--- src/main/scala/scalajsreact/template/pages/AnotherPage.scala
+++ src/main/scala/scalajsreact/template/pages/AnotherPage.scala
@@ -11,15 +11,23 @@ object AnotherPage {
   object Style extends StyleSheet.Inline {
     import dsl._
     val content: StyleA = style(textAlign.center,
-                        fontSize(30.px),
+                        fontSize(50.px),
                         minHeight(450.px),
                         paddingTop(40.px))
   }
 
   val component: Component[Unit, Unit, Unit, CtorType.Nullary] =
-    ScalaComponent.builder
-      .static("AnotherPage")(<.div(^.id:="mikey", Style.content, "Another page by MIKE"))
-      .build
+
+    ScalaComponent.builder[Unit]("TodoList")
+      //.initialState(State(Nil, ""))
+      .render_P(p => <.div(^.id:="mikey", Style.content, "MORE STUFF FROM MIKE"))
+      .componentDidMount(s => Callback.lift(() => {
+        println("ANOTHER PAGE componentDidMount()")
+      })).build
+
+    //ScalaComponent.builder
+    //  .static("AnotherPage")(<.div(^.id:="mikey", Style.content, "Another page by MIKE"))
+    //  .build
 
   def apply() = component()
 }
